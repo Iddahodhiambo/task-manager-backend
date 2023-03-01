017e00e755d7febf10184d3c408031558fc0a695
Iddahodhiambo <iddahawuor@yahoo.com>
2023-03-01 22:33:08 +0300

    create models,routes and task methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,34 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  get '/tasks' do
+    tasks = tasks.all
+    tasks.to_json
+  end
+
+
+  get '/tasks/:id' do
+    task = tasks.find(params[:id])
+    task.to_json
+  end
+
+  post '/tasks' do
+    task = Task.create(
+      title: params[:title],
+      description: params[:description],
+      due_date: params[:due_date],
+      completion_status: params[:completion_status]
+    )
+    task.to_json
+  end
+  put '/tasks/:id' do
+    task = Task.find(params[:id])
+    task.update(title: params[:title],
+    description: params[:description],
+    due_date: params[:due_date],
+    completion_status: params[:completion_status]
+    )
+    task.to_json
+  end
+
 end
